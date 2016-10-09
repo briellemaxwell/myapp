f6fe73ee541d153e82efaa78ab0b2a0b1ac8839a
“Brielle <briellemaxwell@gmail.com>
2016-10-09 09:05:16 -0400

    Add image kind
    Allow for image post

---
+++ db/migrate/20161009034253_add_kind_to_posts.rb
@@ -0,0 +1,4 @@
+class AddKindToPosts < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
---
+++ db/migrate/20161009034557_add_kinds_to_posts.rb
@@ -0,0 +1,5 @@
+class AddKindsToPosts < ActiveRecord::Migration[5.0]
+  def change
+    add_column :posts, :kind, :string
+  end
+end
